93496ff7a42e85c92103ad350f513486945bc91d
Daniil Petrenko <daniilp162@gmail.com>
2023-03-29 21:21:59 +0500

    hide keyboard

--- AddScreen.js
+++ AddScreen.js
@@ -1,50 +1,58 @@
-import {SafeAreaView, StyleSheet,TextInput, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet,TextInput, Text, View, Keyboard,  TouchableWithoutFeedback } from 'react-native';
 import * as React from "react";
-import {useState} from "react";
-import * as Font from "expo-font"
-import AppLoading from "expo-app-loading";
-import {Picker} from '@react-native-picker/picker'
+import {useCallback, useState} from "react";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
-const fonts = () => { Font.loadAsync({
-    'mt-bold':require('./assets/fonts/Montserrat-Bold.ttf'),
-    'mt-light':require('./assets/fonts/Montserrat-Light.ttf')
-});
-  
-}
+const HideKeyboard = ({ children }) => (
+    <TouchableWithoutFeedback
+        onPress={() => Keyboard.dismiss()}>
+        {children}
+    </TouchableWithoutFeedback>
+);
 export default function AddScreen() {
+
     const [name, setName] = useState('shaun');
     const [age, setAge] = useState('30');
-    const [font, setFont] = useState(false);
-    const [selectedLanguage, setSelectedLanguage] = useState();
-    if (font) {
+    const [fontsLoaded] = useFonts({
+        // 'Montserrat-Light': require('./assets/fonts/Montserrat-Light.ttf'),
+        'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+    });
 
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
 
-        return (
-            <View style={styles.container}>
-                <Text style={styles.title}>Сообщить о нарушении</Text>
+    // if (!fontsLoaded) {
+    //     return null;
+    // }
+
+    return (
+        <HideKeyboard>
+            <View style={styles.container} onLayout={onLayoutRootView}>
+                <Text style={{fontFamily: 'Montserrat-Bold', fontSize: 27}}>Сообщить о нарушении</Text>
+
                 <TextInput
-                    placeholder='e.g. John Doe'
-                    style={styles.input}
+                    placeholder='Введите загловок'
+                    style={styles.inputTitle}
                     onChangeText={(value) => setName(value)}/>
 
-                {/*<Text>Enter age:</Text>*/}
+
                 <TextInput
-                    placeholder='e.g. 99'
-                    style={styles.input}
+                    multiline
+                    numberOfLines={6}
+                    placeholder='Опишите проблему подробнее'
+                    style={styles.inputDescription}
                     onChangeText={(value) => setAge(value)}/>
 
                 <Text style={styles.result}>name: {name}, age: {age}</Text>
             </View>
-        );
-    }
-    else {
-        return (
-            <AppLoading startAsync={fonts}
-                        onFinish={() => setFont(true)}
-                        onError = {console.warn()}
-            />
-        )
-    }
+
+        </HideKeyboard>
+    );
+
 }
 
 const styles = StyleSheet.create({
@@ -54,24 +62,37 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         // justifyContent: 'center',
     },
-    input: {
+    inputTitle: {
         justifyContent:'center',
         borderWidth: 1,
         borderColor: '#777',
         marginTop:25,
         padding: 8,
         margin: 10,
+        // fontFamily:'mt-light',
         width: 300,
+    },
+    inputDescription: {
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: '#777',
+        marginTop:25,
+        padding: 8,
+        margin: 10,
+        // fontFamily:'mt-light',
+        width: 300,
+        height:200
     },
     title:{
         justifyContent:'center',
         fontWeight:'bold',
-        fontFamily:'mt-bold',
+       // fontFamily:'mt-light',
         fontSize:30,
         marginTop:30,
         color:'#000000',
 
-    }
+    },
+
 });
 
 

56704c294dcee59fa702b8abfae0e2c9916b5030
Daniil Petrenko <daniilp162@gmail.com>
2023-05-10 18:23:16 +0500

    modal window

--- FeedScreen.js
+++ FeedScreen.js
@@ -64,12 +64,32 @@ export default function FeedScreen()
                     transparent={true}
                     visible={modalVisible}
                     onRequestClose={() => {
-                        alert('Modal has been closed.');
+
                         setModalVisible(!modalVisible);
                     }}>
                     <View style={styles.сenteredView}>
                         <View style={styles.modalView}>
-                            <Text style={styles.modalText}>Hello World!</Text>
+                            <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+                                <AntDesign style={styles.closeButton} name='close'></AntDesign>
+                            </TouchableOpacity>
+                            <Text style={styles.modalTitle}>Неубранный снег</Text>
+                            <Image style={styles.imageModal}
+                                   source = {require('./assets/snow_img.png')}/>
+                            <Text style={styles.modalAddress}>Адрес</Text>
+                            <Text style={styles.modalStreet}>Ленина</Text>
+                            <Text style={styles.modalDate}>01.01.2023</Text>
+                            <Text style={styles.modalDescTitle}>Описание</Text>
+                            <Text style={styles.modalDesc}>Собрали снег в большую кучу, но не вывозят уже 5й день!</Text>
+                            <View style={styles.modalStatus}>
+                                <Text style={{textAlign:'center',paddingTop:5}}>Акутально</Text>
+
+                            </View>
+                            <TouchableOpacity onPress={handleLikePress}>
+                                {/*return <Ionicons name={liked} />;*/}
+
+                                <Text style = {styles.textLikeModal} >{liked ? <AntDesign style={styles.likesModal} name = 'like1'/> :<AntDesign style={styles.dislike} name = 'like2'/>  } </Text>
+                            </TouchableOpacity>
+                        {/*Собрали снег в большую кучу, но не вывозят уже 5й день!   */}
                         </View>
                     </View>
                 </Modal>
@@ -96,9 +116,17 @@ const styles = StyleSheet.create(
         borderColor:'rgba(84,74,74,0.39)',
         width:370,
         height:200,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+
+
 
         },
         shadow:{
+
             // shadowOpacity:1,
             // shadowRadius:5,
             // shadowOffset:{width:0,height:50}
@@ -153,14 +181,14 @@ const styles = StyleSheet.create(
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
-            marginTop: 22,
+            marginTop: 0,
         },
         modalView: {
             margin: 20,
             backgroundColor: 'white',
             borderRadius: 20,
             padding: 35,
-            alignItems: 'center',
+            // alignItems: 'center',
             shadowColor: '#000',
             shadowOffset: {
                 width: 0,
@@ -169,27 +197,105 @@ const styles = StyleSheet.create(
             shadowOpacity: 0.25,
             shadowRadius: 4,
             elevation: 5,
+            width:370,
+            height:450
         },
-        button: {
-            borderRadius: 20,
-            padding: 10,
-            elevation: 2,
+
+        closeButton:{
+            fontSize:30,
+            position:'absolute',
+            width:30,
+            marginLeft:295,
+            marginTop:-20
         },
-        buttonOpen: {
-            backgroundColor: '#F194FF',
+        modalTitle: {
+            position:'absolute',
+            fontSize:28,
+            paddingTop:13,
+            marginLeft:12,
+            width:230
         },
-        buttonClose: {
-            backgroundColor: '#2196F3',
+        imageModal:{
+            position:'absolute',
+            width:350,
+            height:180,
+            marginTop:60,
+            marginLeft:10,
+            borderRadius:20
         },
-        textStyle: {
-            color: 'white',
-            fontWeight: 'bold',
-            textAlign: 'center',
+        modalAddress: {
+            position:'absolute',
+            fontSize:16,
+            paddingTop:250,
+            // fontWeight:'500',
+            paddingLeft:12,
+            fontWeight:'bold',
+            // marginRight:0,
+            // marginHorizontal:9
+
+
         },
-        modalText: {
-            marginBottom: 15,
-            textAlign: 'center',
+        modalStreet: {
+            position:'absolute',
+            fontSize:16,
+            paddingTop:270,
+            fontWeight:'500',
+            paddingLeft:12,
+            // fontWeight:'bold',
         },
+        modalDate: {
+            position:'absolute',
+            fontSize:16,
+            fontWeight:'500',
+            paddingTop:290,
+            paddingLeft:12,
+            color:'rgba(33,28,28,0.55)'
+        },
+        modalDescTitle: {
+            position:'absolute',
+            fontSize:16,
+            paddingTop:310,
+            paddingLeft:12,
+            fontWeight:'bold',
+            color:'rgb(0,0,0)'
+        },
+        modalDesc: {
+            position:'absolute',
+            fontSize:16,
+            paddingTop:330,
+            paddingLeft:12,
+            fontWeight:'500',
+            color:'rgb(0,0,0)'
+        },
+        modalStatus:{
+            backgroundColor:'#D9D9D9',
+            borderRadius:20,
+            height:30,
+            width:90,
+            position:'absolute',
+            fontSize:16,
+            marginTop:380,
+            marginLeft:13,
+            fontWeight:'500',
+            color:'rgb(0,0,0)'
+        },
+        textLikeModal: {
+            position:'absolute',
+            marginHorizontal: 9,
+            paddingTop:339,
+            paddingLeft:80,
+            fontSize:35,
+            color:'rgba(53,145,53,0.71)'
+        },
+        likesModal: {
+            position:'absolute',
+            marginHorizontal: 9,
+            paddingLeft:80,
+            paddingTop:100,
+            fontSize:35,
+            color:'rgba(53,145,53,0.71)'}
+
+
     });
 
 
